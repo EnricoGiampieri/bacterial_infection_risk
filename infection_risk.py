9c4903da4a87b90009ceff88c3dab285046318b4
Enrico Giampieri <enrico.giampieri@unibo.it>
2023-11-29 10:16:01 +0100

    aggiustati titoli grafici e scala dei parametri

--- infection_risk.py
+++ infection_risk.py
@@ -6,10 +6,15 @@ import numpy as np
 
 st.title('Public health risk arising from the presence of _Campylobacter spp._ in the broiler meat chain')
 
-x_low, x_top = st.sidebar.slider('_Campylobacter spp._ prevalence in broiler flocks sent to slaughter', 0.0, 1.0, (0.45, 0.55))
-z_low, z_top = st.sidebar.slider('_Campylobacter spp._ concentration in broiler caeca content', 0.0, 3.0, (0.5, 1.0))
-t_low, t_top = st.sidebar.slider('Probability of exposure to an infectious dose for the population of interest', 0.0, 1.0, (0.45, 0.55))
+x_low, x_top = st.sidebar.slider('_Campylobacter spp._ prevalence in broiler flocks sent to slaughter', 0, 100, (45, 55))
+z_low, z_top = st.sidebar.slider('_Campylobacter spp._ concentration in broiler caeca content', 0.0, 7.0, (0.5, 1.0))
+t_low, t_top = st.sidebar.slider('Probability of exposure to an infectious dose for the population of interest', 0, 100, (45, 55))
 
+x_low /= 100.0
+t_low /= 100.0
+
+x_top /= 100.0
+t_top /= 100.0
 
 x_base = np.linspace(0, 7, 101)
 y_base = x_base/(x_base+1)
@@ -24,7 +29,7 @@ v[x_base>v_top] = np.nan
 
 st.write('### public health risk as a function of _Campylobacter spp._ concentration in broiler caeca content')
 # if I put the dot after the name, it crashes the graphics...
-x_label = "average Campylobacter spp bacterial load ( Log10 cfu/g)"
+x_label = "Campylobacter spp concentration (Log10 cfug/g)"
 chart_data = pd.DataFrame({
     x_label: x_base,
     'risk function (%)': y_base*100,
@@ -50,7 +55,7 @@ v[x_base<v_low] = np.nan
 v[x_base>v_top] = np.nan
 
 st.write('### public health risk as a function of _Campylobacter spp._ prevalence in broiler flocks')
-x_label = "average Campylobacter exposure probability"
+x_label = "Campylobacter spp prevalence"
 chart_data = pd.DataFrame({
     x_label: x_base*100,
     'risk function (%)': y_base*100,
